fe14ad818158fa42f27e8da9c01a1c9b19919bd6
Kamal Bennani <kamalbouchboy1@hotmail.fr>
2017-11-14 12:41:08 +0100

    final version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import { Input, Icon } from 'antd';
-
-import 'antd/dist/antd.css'; 
+import io from 'socket.io-client';
 import './App.css';
 
 class MessageItem extends Component {
@@ -9,10 +7,15 @@ class MessageItem extends Component {
     const message = this.props.message;
     return (
       <div className="MessageItem">
-        <div className="MessageItem__user"></div>
-        <div className="MessageItem__user">{message.user}</div>
-        <div className="MessageItem__body">{message.body}</div>
-        <div className="MessageItem__date">{message.date}</div>
+        <div className="MessageItem__user" />
+        <div className="MessageItem__content">
+          <div className="MessageItem__body">
+            <div className="MessageItem__title">{message.from}</div>
+            <div className="MessageItem__text">{message.body}</div>
+            {message.img && <div className="MessageItem__img"><img src={message.img} /></div>}
+          </div>
+          <div className="MessageItem__date">{message.date}</div>
+        </div>
       </div>
     )
   }
@@ -20,7 +23,16 @@ class MessageItem extends Component {
 
 class App extends Component {
   state = {
+    inputValue: '',
     messages: [],
+  }
+
+  componentDidMount() {
+    this.socket = io('http://1b850b86.ngrok.io/');
+    console.log('this.socket', this.socket);
+    this.socket.on('new received message', (data) => {
+      this.addMessage(data);
+    });
   }
 
   handleSubmit = (event) => {
@@ -28,27 +40,51 @@ class App extends Component {
       const value = event.target.value;
       const messages = this.state.messages;
       const message = {
+        from: 'me',
         body: value,
         date: new Date().toString(),
       }
-    
+
+      this.socket.emit('new message', message);
+      this.addMessage(message);
       this.setState({
-        messages: messages.concat(message),
-      })
+        inputValue: '',
+      });
     }
   }
 
+  addMessage = (message) => {
+    const messages = this.state.messages;
+    this.setState({
+      messages: messages.concat(message),
+    });
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      inputValue: event.target.value,
+    });
+  }
+
   render() {
+    const inputValue = this.state.inputValue;
     const messages = this.state.messages;
     return (
       <div className="App">
-        <div className="Input">
-          <Input prefix={<Icon type="user" />} placeholder="Type a message" onKeyDown={this.handleSubmit} />
+        <div className="App__header">
+          Messenger (+33644604495)
         </div>
-        <div className="MessageList">
-          {messages.map(function(message) {
-            return <MessageItem message={message} />;
-          })}
+        <div className="App__content">
+          <div className="MessageList">
+            {messages.map(function(message, key) {
+              return <MessageItem key={key} message={message} />;
+            })}
+          </div>
+        </div>
+        <div className="App__footer">
+          <div className="input">
+            <input placeholder="Type a message" value={inputValue} onChange={this.handleChange} onKeyDown={this.handleSubmit} />
+          </div>
         </div>
       </div>
     );
